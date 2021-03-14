a68b3977efcbc316e0bcec3b0f8d9bb4431b6095
Rob <whrathgar@gmail.com>
2021-03-13 20:00:30 -0500

    hello world godot

---
+++ Godot_Tutorials/Hello_World/HelloWorld.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var question = "What are we waiting for? "
+	var answer = "GODOT!"
+	print(question + answer)
